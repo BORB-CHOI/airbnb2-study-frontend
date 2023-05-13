3b2b9200216b24ed8e572d94f30a36ba4c9f1412
BORB <qzsec299@naver.com>
2023-05-13 23:03:29 +0900

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/lib/useUser.ts

--- src/lib/useUser.ts
+++ src/lib/useUser.ts
@@ -3,6 +3,7 @@ import { getMe } from "../api";
 import { IUser } from "../types";
 
 export default function useUser() {
+  // 'me'라는 키로 캐시를 저장하고, getMe 함수를 실행한다. 이제 언제든지 'me'라는 키로 캐시를 불러올 수 있다.
   const { isLoading, data, isError } = useQuery<IUser>(["me"], getMe, {
     retry: false,
     refetchOnWindowFocus: false,
